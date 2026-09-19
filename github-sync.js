545a9c26d98149839023f8e58c73c5a49a544797
Bảo vệ an ninh mạng <165423654+o0oS2@users.noreply.github.com>
2026-09-19 23:22:10 +0700

    Add files via upload

--- github-sync.js
+++ github-sync.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api-chamcong.o0os2.workers.dev";
+const API_URL = "https://tinhluong.o0os2.workers.dev";
 
 let currentUser = localStorage.getItem("cc_currentUser") || null;
 let saveTimer = null;
